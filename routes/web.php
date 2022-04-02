73f593a5a23f176eda87e1bbcefa4eed442deb10
tadas <tdavidsonas88@gmail.com>
2022-04-02 09:01:09 +0300

    Use caching for expensive operations.

--- routes/web.php
+++ routes/web.php
@@ -18,20 +18,15 @@ Route::get('/', function () {
 });
 
 Route::get('posts/{post}', function ($slug){
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
-
-    ddd($path);
-
-    if (!file_exists($path)) {
+    if (!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
         return redirect('/');
-
-//        abort(404);
     }
 
-    $post = file_get_contents($path);
+    $post = cache()->remember("posts.{slug}", now()->addMinutes(20), function () use ($path){
+        return file_get_contents($path);
+    });
 
-    return view('post', [
-        'post' => $post,
-    ]);
+
+    return view('post', ['post' => $post]);
 })->where('post', '[A-z_\-]+');
 

17c36beabe2fbac94cc7a3dfb1b838f304936a7b
tadas <tdavidsonas88@gmail.com>
2022-04-03 08:39:12 +0300

    yaml-front-matter composer package for Post metadata. Using collection.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,27 +4,56 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post
 {
+    public $title; // My First Post => my-first-post
+
+    public $excerpt;
+
+    public $date;
+
+    public $body;
+
+    public $slug;
+
+    /**
+     * @param $title
+     * @param $excerpt
+     * @param $date
+     * @param $body
+     * @param $slug
+     */
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
+    }
+
     public static function all()
     {
-        $files = File::files(resource_path("posts/"));
-
-        return array_map(function ($file) {
-            return $file->getContents();
-        }, $files);
+        return collect(File::files(resource_path("posts")))
+            ->map(function ($file) {
+                return YamlFrontMatter::parseFile($file);
+            })
+            ->map(function ($document) {
+                return new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug
+                );
+            });
     }
 
     public static function find($slug)
     {
-        if (!file_exists($path =resource_path("posts/{$slug}.html"))) {
-            throw new ModelNotFoundException();
-        }
-
-        return cache()->remember("posts.{slug}", now()->addMinutes(20), function () use ($path) {
-            return file_get_contents($path);
-        });
-
+        // Of all of the blog posts, find the one with a slug that matches the one that was requested.
+        return static::all()->firstWhere('slug', $slug);
     }
 }

75ca9362a621a35346923b382a7802abf8514386
tadas <tdavidsonas88@gmail.com>
2022-04-03 07:16:46 +0300

    Use the FileSystem class to read a directory.

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-//    return Post::find('my-first-post');
-    return view('posts');
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
 });
 
 Route::get('posts/{post}', function ($slug){

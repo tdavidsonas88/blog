9323bad1f77eeee44cca0ffe2f0df955652a3d44
tadas <tdavidsonas88@gmail.com>
2022-04-07 17:24:19 +0300

    Learning the blade basics.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,16 +4,21 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-    <?php foreach ($posts as $post) : ?>
-        <article>
+
+    @if(true)
+        <h1>Just learning the Blade basics</h1>
+    @endif
+
+    @foreach ($posts as $post)
+        <article class="{{ $loop->even ? 'foobar' : '' }}">
             <h1>
-                <a href="/posts/<?= $post->slug; ?>">
-                    <?= $post->title; ?>
+                <a href="/posts/{{ $post->slug }}">
+                    {{ $post->title }}
                 </a>
             </h1>
             <div>
-                <?= $post->excerpt; ?>
+                {{ $post->excerpt }}
             </div>
         </article>
-    <?php endforeach; ?>
+    @endforeach
 </body>
--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,3 @@ Route::get('posts/{post}', function ($slug){
         'post' => Post::find($slug)
     ]);
 })->where('post', '[A-z_\-]+');
-

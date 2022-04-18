4a79d93b3fc1a5008946e09b50dfcdac6a6a29a5
tadas <tdavidsonas88@gmail.com>
2022-04-18 08:00:32 +0300

    Make a post model and migration.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,73 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public $title; // My First Post => my-first-post
-
-    public $excerpt;
-
-    public $date;
-
-    public $body;
-
-    public $slug;
-
-    /**
-     * @param $title
-     * @param $excerpt
-     * @param $date
-     * @param $body
-     * @param $slug
-     */
-    public function __construct($title, $excerpt, $date, $body, $slug)
-    {
-        $this->title = $title;
-        $this->excerpt = $excerpt;
-        $this->date = $date;
-        $this->body = $body;
-        $this->slug = $slug;
-    }
-
-    public static function all()
-    {
-        return cache()->rememberForever('posts.all', function () {
-            return collect(File::files(resource_path("posts")))
-                ->map(function ($file) {
-                    return YamlFrontMatter::parseFile($file);
-                })
-                ->map(function ($document) {
-                    return new Post(
-                        $document->title,
-                        $document->excerpt,
-                        $document->date,
-                        $document->body(),
-                        $document->slug
-                    );
-                })
-                ->sortByDesc('date');
-        });
-    }
-
-    public static function find($slug)
-    {
-        // Of all of the blog posts, find the one with a slug that matches the one that was requested.
-        return static::all()->firstWhere('slug', $slug);
-    }
-
-    public static function findOrFail($slug)
-    {
-        // Of all of the blog posts, find the one with a slug that matches the one that was requested.
-        $post = static::find($slug);
-
-        if (!$post) {
-            throw new ModelNotFoundException();
-        }
-
-        return $post;
-    }
+    use HasFactory;
 }

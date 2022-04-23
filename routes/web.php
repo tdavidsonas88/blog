fef0a0eb1de6ab2f39a104ff160828ae1fa8b6ef
tadas <tdavidsonas88@gmail.com>
2022-04-23 08:24:19 +0300

    Show all category posts.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
@@ -25,3 +26,9 @@ Route::get('posts/{post}', function ($id){
         'post' => Post::findOrFail($id)
     ]);
 })->whereAlphaNumeric('post');
+
+Route::get('categories/{category:slug}', function (Category $category){
+    return view('posts', [
+        'posts' => $category->posts,
+    ]);
+});

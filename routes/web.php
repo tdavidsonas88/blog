76a2a9503e5e4b23442c7bb2ea64ad4347ffe7d3
tadas <tdavidsonas88@gmail.com>
2022-04-24 14:42:52 +0300

    View all post by an author.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
@@ -22,7 +23,7 @@ Route::get('/', function () {
     });
 
     return view('posts', [
-        'posts' => Post::with('category')->get()
+        'posts' => Post::latest()->with('category', 'author')->get()
     ]);
 });
 
@@ -35,5 +36,11 @@ Route::get('posts/{post}', function ($id){
 Route::get('categories/{category:slug}', function (Category $category){
     return view('posts', [
         'posts' => $category->posts,
+    ]);
+});
+
+Route::get('authors/{author:username}', function (User $author){
+    return view('posts', [
+        'posts' => $author->posts,
     ]);
 });

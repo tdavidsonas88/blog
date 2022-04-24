f4d6754c4d25260cbf6dfadcede4027199b13001
tadas <tdavidsonas88@gmail.com>
2022-04-24 17:06:43 +0300

    Eager load relationships on existing model.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,6 +11,8 @@ class Post extends Model
 
     protected $fillable = ['user_id', 'category_id', 'title', 'slug', 'excerpt', 'body'];
 
+    protected $with = ['category', 'author'];
+
     public function category()
     {
         return $this->belongsTo(Category::class);
